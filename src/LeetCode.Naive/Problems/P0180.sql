2da2cabde94be57fc8333a10fc437c704bb54007
Viacheslav Zholnovach <vyajol@gmail.com>
2019-11-22 09:25:35 +0200

    P0180

---
+++ src/LeetCode.Naive/Problems/P0180.sql
@@ -0,0 +1,12 @@
+﻿/*
+Problem: https://leetcode.com/problems/consecutive-numbers/
+Submission: https://leetcode.com/submissions/detail/280812801/
+*/
+
+/* Write your T-SQL query statement below */
+select distinct(a.Num) as ConsecutiveNums
+from Logs a
+left join Logs b on a.Id = b.Id - 1
+left join Logs c on a.Id = c.Id - 2
+where a.Num = b.Num
+  and a.Num = c.Num
