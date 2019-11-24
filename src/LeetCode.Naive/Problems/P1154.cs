af5ff0f3399194deec06cb9a05a2bd03bdd47660
Viacheslav Zholnovach <vyajol@gmail.com>
2019-11-24 16:47:55 +0200

    P1154

---
+++ src/LeetCode.Naive/Problems/P1154.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/day-of-the-year/
+	///		Submission: https://leetcode.com/submissions/detail/281318166/
+	/// </summary>
+	internal class P1154
+	{
+		public int DayOfYear(string date)
+		{
+			var input = DateTime.Parse(date);
+
+			var daysInMonth = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			if (input.Year % 4 == 0 && input.Year % 100 != 0)
+				daysInMonth[1] = 29;
+
+			return input.Day + ((input.Month > 1) ? daysInMonth.Take(input.Month - 1).Sum() : 0);
+		}
+	}
+}
