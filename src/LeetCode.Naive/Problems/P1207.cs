e2b4514c5ff90f59814f93ae5f96588cc9c3eb8c
Viacheslav Zholnovach <vyajol@gmail.com>
2019-11-24 21:32:12 +0200

    P1207

---
+++ src/LeetCode.Naive/Problems/P1207.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/unique-number-of-occurrences/
+	///		Submission: https://leetcode.com/submissions/detail/281364421/
+	/// </summary>
+	internal class P1207
+	{
+		public bool UniqueOccurrences(int[] arr)
+		{
+			var map = arr.GroupBy(a => a)
+					.ToDictionary(a => a.Key, a => a.Count());
+
+			return map.Values.Count == map.Values.Distinct().Count();
+		}
+	}
+}
