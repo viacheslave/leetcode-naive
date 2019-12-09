b1261bca1843000fd88ddcfeed28dce273c74958
Viacheslav Zholnovach <vyajol@gmail.com>
2019-12-09 17:07:06 +0200

    P1282

---
+++ src/LeetCode.Naive/Problems/P1282.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
+	///		Submission: https://leetcode.com/submissions/detail/284802613/
+	/// </summary>
+	internal class P1282
+	{
+		public IList<IList<int>> GroupThePeople(int[] groupSizes)
+		{
+			var buckets = new Dictionary<int, List<int>>();
+
+			for (int index = 0; index < groupSizes.Length; index++)
+			{
+				if (!buckets.ContainsKey(groupSizes[index]))
+					buckets[groupSizes[index]] = new List<int>();
+
+				buckets[groupSizes[index]].Add(index);
+			}
+
+			var ans = new List<IList<int>>();
+
+			foreach (var bucket in buckets)
+				ans.AddRange(Split(bucket));
+
+			return ans;
+		}
+
+		private IEnumerable<IList<int>> Split(KeyValuePair<int, List<int>> bucket)
+		{
+			var res = new List<IList<int>>();
+
+			for (int i = 0; i < bucket.Value.Count; i += bucket.Key)
+				res.Add(bucket.Value.Skip(i).Take(bucket.Key).ToList());
+
+			return res;
+		}
+	}
+}
