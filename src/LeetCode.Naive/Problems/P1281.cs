7ea1ee8c6907c48569fd4f07eb7695036e1cbd50
Viacheslav Zholnovach <vyajol@gmail.com>
2019-12-09 16:36:41 +0200

    P1281

---
+++ src/LeetCode.Naive/Problems/P1281.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+	///		Submission: https://leetcode.com/submissions/detail/284798553/
+	/// </summary>
+	internal class P1281
+	{
+		public int SubtractProductAndSum(int n)
+		{
+			var digits = n.ToString()
+				.Select(_ => int.Parse(_.ToString()))
+				.ToList();
+
+			return digits.Aggregate((a, b) => a * b) - digits.Sum();
+		}
+	}
+}
