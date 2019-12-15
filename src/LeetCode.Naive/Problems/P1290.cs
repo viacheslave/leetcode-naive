e5e2def62c960486df473f53cd3df15cb24b5c82
Viacheslav Zholnovach <vyajol@gmail.com>
2019-12-15 09:00:11 +0200

    P1290

---
+++ src/LeetCode.Naive/Problems/P1290.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+	///		Submission: https://leetcode.com/submissions/detail/286082021/
+	/// </summary>
+	internal class P1290
+	{
+		public int GetDecimalValue(ListNode head)
+		{
+			var ans = 0;
+
+			var current = head;
+			while (current != null)
+			{
+				ans *= 2;
+				ans += current.val;
+				current = current.next;
+			}
+
+			return ans;
+		}
+	}
+}
