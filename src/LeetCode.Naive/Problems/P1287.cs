84f3eece12a2ca0038c0c17c8e9c3e6ffd8d41cb
Viacheslav Zholnovach <vyajol@gmail.com>
2019-12-15 08:53:38 +0200

    P1287

---
+++ src/LeetCode.Naive/Problems/P1287.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+	///		Submission: https://leetcode.com/submissions/detail/286080836/
+	/// </summary>
+	internal class P1287
+	{
+		public int FindSpecialInteger(int[] arr)
+		{
+			var thres = arr.Length / 4;
+
+			return arr.GroupBy(x => x)
+					.ToDictionary(x => x.Key, x => x.Count())
+					.Single(x => x.Value > thres)
+					.Key;
+		}
+	}
+}
