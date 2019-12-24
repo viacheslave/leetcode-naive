36e2aa76ca852af123514787cb048a8f1120e08f
Viacheslav Zholnovach <v.zholnovach@plarium.com>
2019-12-24 15:14:45 +0200

    P1295

---
+++ src/LeetCode.Naive/Problems/P1295.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+	///		Submission: https://leetcode.com/submissions/detail/288229785/
+	/// </summary>
+	internal class P1295
+	{
+		public int FindNumbers(int[] nums)
+		{
+			return nums.Count(num => num.ToString().Length % 2 == 0);
+		}
+	}
+}
