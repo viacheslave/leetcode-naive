5ca90148c26d87fe1f96bb7c08ca0338d0e79110
Viacheslav Zholnovach <vyajol@gmail.com>
2019-12-28 20:31:32 +0200

    P1233

---
+++ src/LeetCode.Naive/Problems/P1233.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/
+	///		Submission: https://leetcode.com/submissions/detail/289255940/
+	/// </summary>
+	internal class P1233
+	{
+		public IList<string> RemoveSubfolders(string[] folder)
+		{
+			Array.Sort(folder);
+
+			var ans = new List<string>();
+
+			foreach (var item in folder)
+			{
+				if (ans.Count == 0)
+				{
+					ans.Add(item);
+					continue;
+				}
+
+				var last = ans[ans.Count - 1];
+				if (item.IndexOf(last) == 0 && item[last.Length] == '/')
+					continue;
+
+				ans.Add(item);
+			}
+
+			return ans;
+		}
+	}
+}
