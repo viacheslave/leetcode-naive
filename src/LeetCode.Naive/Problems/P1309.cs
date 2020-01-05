4098c9b2a93d205f5b42a6a26cc60efadce3d822
Viacheslav Zholnovach <vyajol@gmail.com>
2020-01-05 08:52:48 +0200

    P1309

---
+++ src/LeetCode.Naive/Problems/P1309.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+	///		Submission: https://leetcode.com/submissions/detail/291351743/
+	/// </summary>
+	internal class P1309
+	{
+		public string FreqAlphabets(string s)
+		{
+			var sb = new StringBuilder(s);
+
+			for (int i = s.Length - 1; i >= 0; i--)
+			{
+				if (sb[i] == '#')
+				{
+					sb[i] = (char)(int.Parse(s.Substring(i - 2, 2)) + 96);
+					sb.Remove(i - 2, 2);
+					i -= 2;
+				}
+			}
+
+			for (int i = 0; i < sb.Length; i++)
+			{
+				if (char.IsDigit(sb[i]))
+					sb[i] = (char)(int.Parse(sb[i].ToString()) + 96);
+			}
+
+			return sb.ToString();
+		}
+	}
+}
