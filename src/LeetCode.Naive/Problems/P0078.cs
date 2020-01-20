827f3150a3448d9a0fe90c31a52948e2e3fa581d
Viacheslav Zholnovach <vyajol@gmail.com>
2020-01-20 21:44:16 +0200

    P1324, P1325

--- src/LeetCode.Naive/Problems/P0078.cs
+++ src/LeetCode.Naive/Problems/P0078.cs
@@ -37,33 +37,4 @@ namespace LeetCode.Naive.Problems.Easy
 			}
 		}
 	}
-	/// <summary>
-	///		Problem: https://leetcode.com/problems/print-words-vertically/
-	///		Submission: https://leetcode.com/submissions/detail/295932663/
-	/// </summary>
-	internal class P1324
-	{
-		public IList<string> PrintVertically(string s)
-		{
-			var words = s.Split(' ');
-			var length = words.Max(s1 => s1.Length);
-
-			var mat = new char[words.Length][];
-			for (int i = 0; i < words.Length; i++)
-				mat[i] = words[i].ToCharArray();
-
-			var ans = new List<string>();
-
-			for (int j = 0; j < length; j++)
-			{
-				var arr = new char[words.Length];
-				for (int i = 0; i < words.Length; i++)
-					arr[i] = j < mat[i].Length ? mat[i][j] : ' ';
-
-				ans.Add(new string(arr).TrimEnd());
-			}
-
-			return ans;
-		}
-	}
 }
---
+++ src/LeetCode.Naive/Problems/P1325.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/delete-leaves-with-a-given-value/
+	///		Submission: https://leetcode.com/submissions/detail/295935692/
+	/// </summary>
+	internal class P1325
+	{
+		public TreeNode RemoveLeafNodes(TreeNode root, int target)
+		{
+			var res = Traverse(root, target);
+			if (res)
+				return null;
+
+			return root;
+		}
+
+		private bool Traverse(TreeNode node, int target)
+		{
+			if (node == null)
+				return true;
+
+			var lr = Traverse(node.left, target);
+			var rr = Traverse(node.right, target);
+
+			if (lr)
+				node.left = null;
+			if (rr)
+				node.right = null;
+
+			return lr && rr && node.val == target;
+		}
+	}
+}
