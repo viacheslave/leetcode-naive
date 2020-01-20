074c651868b4526c481661034fc144b848f4cb0c
Viacheslav Zholnovach <vyajol@gmail.com>
2020-01-20 21:04:32 +0200

    P1323

---
+++ src/LeetCode.Naive/Problems/P1323.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/maximum-69-number/
+	///		Submission: https://leetcode.com/submissions/detail/295925341/
+	/// </summary>
+	internal class P1323
+	{
+		public int Maximum69Number(int num)
+		{
+			var index = num.ToString().IndexOf('6');
+			if (index == -1)
+				return num;
+
+			var sb = new StringBuilder(num.ToString());
+			sb[index] = '9';
+			return int.Parse(sb.ToString());
+		}
+	}
+}
