41b7b586eddf9afabf7286f30ded09d17acccd3b
Viacheslav Zholnovach <vyajol@gmail.com>
2020-02-01 20:29:04 +0200

    P0195

---
+++ src/LeetCode.Naive/Problems/P0195.sh
@@ -0,0 +1,15 @@
+﻿# https://leetcode.com/problems/tenth-line/
+# https://leetcode.com/submissions/detail/299297614/
+
+# Read from the file file.txt and output the tenth line to stdout.
+#!/bin/bash
+i=0
+while IFS= read -r line 
+do
+  if [ $i -eq 9 ]  
+  then 
+    echo "$line" 
+    break 
+  fi 
+  ((i=i+1)) 
+done < "file.txt"
