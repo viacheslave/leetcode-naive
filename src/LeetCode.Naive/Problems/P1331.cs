97a2aefb67d4da6e39f88758aec8d7f7d0258454
Viacheslav Zholnovach <vyajol@gmail.com>
2020-02-01 19:26:04 +0200

    P1331

---
+++ src/LeetCode.Naive/Problems/P1331.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/rank-transform-of-an-array/
+	///		Submission: https://leetcode.com/submissions/detail/299284946/
+	/// </summary>
+	internal class P1331
+	{
+		public int[] ArrayRankTransform(int[] arr)
+		{
+			var sorted = arr.Distinct().ToList();
+			sorted.Sort();
+			var map = sorted.Select((el, i) => (el, i + 1)).ToDictionary(x => x.Item1, x => x.Item2);
+
+			return arr.Select(x => map[x]).ToArray();
+		}
+	}
+}
