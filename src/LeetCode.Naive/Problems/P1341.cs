d20831a4506ab544114c9b59e234c0979d4875f6
Viacheslav Zholnovach <vyajol@gmail.com>
2020-02-02 09:33:54 +0200

    P1341

---
+++ src/LeetCode.Naive/Problems/P1341.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+	///		Submission: https://leetcode.com/submissions/detail/299511859/
+	/// </summary>
+	internal class P1341
+	{
+		public int[] KWeakestRows(int[][] mat, int k)
+		{
+			return mat.Select((row, index) => (index, row.Count(r => r == 1)))
+					.OrderBy(x => x.Item2)
+					.ThenBy(x => x.index)
+					.Take(k)
+					.Select(x => x.index)
+					.ToArray();
+		}
+	}
+}
