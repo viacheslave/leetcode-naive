7103fa64d845c8d1c9cdb9e8aa8350523bfbaa81
Viacheslav Zholnovach <vyajol@gmail.com>
2020-02-16 10:37:24 +0200

    P1351

---
+++ c#/Problems/P1351.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+	///		Submission: https://leetcode.com/submissions/detail/303776727/
+	/// </summary>
+	internal class P1351
+	{
+		public int CountNegatives(int[][] grid)
+		{
+			int ans = 0;
+
+			for (int i = 0; i < grid.Length; i++)
+			{
+				for (int j = 0; j < grid[0].Length; j++)
+				{
+					if (grid[i][j] >= 0)
+						continue;
+
+					ans += grid[0].Length - j;
+					break;
+				}
+			}
+
+			return ans;
+		}
+	}
+}
