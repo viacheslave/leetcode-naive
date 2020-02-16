0f1fdf0991d7c5a82ab07c25b240e39c333a9ae1
Viacheslav Zholnovach <vyajol@gmail.com>
2020-02-16 12:04:41 +0200

    P1354

---
+++ c#/Problems/P1354.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/construct-target-array-with-multiple-sums/
+	///		Submission: https://leetcode.com/submissions/detail/303790486/
+	/// </summary>
+	internal class P1354
+	{
+		public bool IsPossible(int[] target)
+		{
+			long sum = target.Sum(x => (long)x);
+
+			while (true)
+			{
+				var max = int.MinValue;
+				var maxIndex = 0;
+
+				for (int i = 0; i < target.Length; i++)
+				{
+					if (target[i] > max)
+					{
+						max = target[i];
+						maxIndex = i;
+					}
+				}
+
+				if (max == 1)
+					return true;
+
+				var restsum = sum - max;
+				var prevval = max - restsum;
+
+				if (prevval < 1)
+					return false;
+
+				target[maxIndex] = (int)prevval;
+				sum = restsum + prevval;
+			}
+
+			return true;
+		}
+	}
+}
