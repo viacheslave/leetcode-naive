83b3008b7062a6a2dd3d3003ea1c603305814ef7
Viacheslav Zholnovach <vyajol@gmail.com>
2020-02-23 20:56:21 +0200

    P1357

---
+++ c#/Problems/P1357.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/apply-discount-every-n-orders/
+	///		Submission: https://leetcode.com/submissions/detail/306159678/
+	/// </summary>
+	internal class P1357
+	{
+    public class Cashier
+    {
+
+      private int _n;
+      private int _current = 0;
+      private int _discount;
+
+      private Dictionary<int, int> _prices;
+
+      public Cashier(int n, int discount, int[] products, int[] prices)
+      {
+        _n = n;
+        _discount = discount;
+        _prices = new Dictionary<int, int>();
+
+        for (var i = 0; i < products.Length; i++)
+          _prices[products[i]] = prices[i];
+      }
+
+      public double GetBill(int[] product, int[] amount)
+      {
+        _current++;
+
+        var bill = 0;
+        for (var i = 0; i < product.Length; i++)
+          bill += _prices[product[i]] * amount[i];
+
+        if (_current % _n != 0)
+          return bill;
+
+        var dis = 1.0 - _discount / 100.0;
+        return bill * dis;
+      }
+    }
+
+    /**
+     * Your Cashier object will be instantiated and called as such:
+     * Cashier obj = new Cashier(n, discount, products, prices);
+     * double param_1 = obj.GetBill(product,amount);
+     */
+  }
+}
