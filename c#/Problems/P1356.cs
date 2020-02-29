030a0284589cd70f3141c3cadc26513033babc7b
Viacheslav Zholnovach <vyajol@gmail.com>
2020-02-29 23:13:22 +0200

    P1356

---
+++ c#/Problems/P1356.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+	///		Submission: https://leetcode.com/submissions/detail/308046668/
+	/// </summary>
+	internal class P1356
+	{
+		public int[] SortByBits(int[] arr)
+		{
+			return arr.OrderBy(Get)
+					.ThenBy(a => a)
+					.ToArray();
+		}
+
+		private int Get(int arg)
+		{
+			var ans = 0;
+
+			while (arg > 0)
+			{
+				if (arg % 2 == 1)
+					ans++;
+
+				arg >>= 1;
+			}
+
+			return ans;
+		}
+	}
+}
