c0841d2ac954e89d022e3e28f745c92131f8ff96
Viacheslav Zholnovach <vyajol@gmail.com>
2020-03-01 16:54:37 +0200

    P1365

---
+++ c#/Problems/P1365.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/submissions/
+	///		Submission: https://leetcode.com/submissions/detail/308303896/
+	/// </summary>
+	internal class P1365
+	{
+		public int[] SmallerNumbersThanCurrent(int[] nums)
+		{
+			var list = nums.ToList();
+			list.Sort();
+
+			var map = new Dictionary<int, int>();
+			var last = -1;
+
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (i == 0)
+				{
+					map[list[i]] = 0;
+					last = 0;
+					continue;
+				}
+
+				if (list[i] != list[i - 1])
+				{
+					map[list[i]] = i;
+					last++;
+				}
+			}
+
+			return nums.Select(n => map[n]).ToArray();
+		}
+	}
+}
