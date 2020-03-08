594d757dfd154700120d759bc3f1c4e2a1b1909e
Viacheslav Zholnovach <vyajol@gmail.com>
2020-03-08 22:25:10 +0200

    P1375

---
+++ c#/Problems/P1375.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/bulb-switcher-iii/
+	///		Submission: https://leetcode.com/submissions/detail/310649291/
+	/// </summary>
+	internal class P1375
+	{
+		public int NumTimesAllBlue(int[] light)
+		{
+			var set = new SortedSet<int>();
+			var ans = 0;
+
+			for (int i = 0; i < light.Length; i++)
+			{
+				set.Add(light[i]);
+				if (set.Min == 1 && set.Max == i + 1)
+					ans++;
+			}
+
+			return ans;
+		}
+	}
+}
