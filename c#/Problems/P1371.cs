88e23d6a4953a211ac4221f68e2abf37acd8c40f
Viacheslav Zholnovach <vyajol@gmail.com>
2020-03-11 22:03:06 +0200

    P1371

---
+++ c#/Problems/P1371.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems.Easy
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/
+	///		Submission: https://leetcode.com/submissions/detail/311593940/
+	/// </summary>
+	internal class P1371
+	{
+    public int FindTheLongestSubstring(string s)
+    {
+      var lp = new int[s.Length + 1];
+      var rp = new int[s.Length + 1];
+
+      //aeiou
+      for (var i = 0; i <= s.Length; i++)
+      {
+        if (i == 0)
+        {
+          lp[i] = 0;
+          rp[i] = 0;
+          continue;
+        }
+
+        var ch = s[i - 1];
+        var chmask = GetCh(ch);
+        lp[i] = lp[i - 1] ^ chmask;
+
+        ch = s[s.Length - i];
+        chmask = GetCh(ch);
+        rp[i] = rp[i - 1] ^ chmask;
+      }
+
+      var all = 0;
+      for (int i = 0; i < s.Length; i++)
+        all ^= GetCh(s[i]);
+
+      var max = 0;
+      for (var start = 0; start <= s.Length; start++)
+      {
+        for (var end = 0; end <= s.Length - start; end++)
+        {
+          if (s.Length - end - start < max)
+            break;
+
+          var ans = all ^ lp[start] ^ rp[end];
+          if (ans == 0)
+          {
+            max = Math.Max(max, s.Length - end - start);
+            break;
+          }
+        }
+      }
+
+      return max;
+    }
+  }
+}
