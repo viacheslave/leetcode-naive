3ea8b48989cf9dd5df0cdf5894f8d67d86a962bd
Viacheslav Zholnovach <vyajol@gmail.com>
2020-03-14 18:33:47 +0200

    P0652

---
+++ c#/Problems/P0652.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/find-duplicate-subtrees/
+	///		Submission: https://leetcode.com/submissions/detail/312359967/
+	/// </summary>
+	internal class P0652
+	{
+		public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+		{
+			var map = new Dictionary<string, List<TreeNode>>();
+			Traverse(root, map);
+
+			return map.Where(v => v.Value.Count > 1).Select(v => v.Value[0]).ToList();
+		}
+
+		private string Traverse(TreeNode node, Dictionary<string, List<TreeNode>> map)
+		{
+			if (node == null)
+				return "";
+
+			var left = Traverse(node.left, map);
+			var right = Traverse(node.right, map);
+
+			var value = node.val.ToString();
+			if (left != null) value += $"l{left}";
+			if (right != null) value += $"r{right}";
+
+			if (!map.ContainsKey(value))
+				map[value] = new List<TreeNode>();
+
+			map[value].Add(node);
+
+			return value;
+		}
+	}
+}
