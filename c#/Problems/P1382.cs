d3e683b8fd38e983331622e56e18ecb7bfaf7a11
Viacheslav Zholnovach <vyajol@gmail.com>
2020-03-15 12:29:50 +0200

    P1382

---
+++ c#/Problems/P1382.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/balance-a-binary-search-tree/
+	///		Submission: https://leetcode.com/submissions/detail/312640224/
+	/// </summary>
+	internal class P1382
+	{
+		public TreeNode BalanceBST(TreeNode root)
+		{
+			var list = new List<TreeNode>();
+			FillNodes(root, list);
+
+			list = list.OrderBy(e => e.val).ToList();
+
+			return GetMiddle(list);
+		}
+
+		private TreeNode GetMiddle(List<TreeNode> list)
+		{
+			if (list.Count == 0)
+				return null;
+
+			if (list.Count == 1)
+				return new TreeNode(list[0].val);
+
+			var middle = list.Count / 2;
+
+			var middleNode = new TreeNode(list[middle].val);
+			middleNode.left = GetMiddle(list.Take(middle).ToList());
+			middleNode.right = GetMiddle(list.Skip(middle + 1).ToList());
+
+			return middleNode;
+		}
+
+		private void FillNodes(TreeNode node, List<TreeNode> list)
+		{
+			if (node == null) return;
+
+			list.Add(node);
+			FillNodes(node.left, list);
+			FillNodes(node.right, list);
+		}
+	}
+}
