3b5c587ec3cc15fbed861cc6ad7fb4cd5519c357
Viacheslav Zholnovach <vyajol@gmail.com>
2020-03-22 00:22:35 +0200

    P1386

---
+++ java/problems/P1386.java
@@ -0,0 +1,67 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+/*
+ * Problem: https://leetcode.com/problems/cinema-seat-allocation/
+ * Submission: https://leetcode.com/submissions/detail/314593738/
+ */
+class ProductOfNumbers {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+    var map = new HashMap<Integer, HashSet<Integer>>();
+
+    for (int i = 0; i < reservedSeats.length; i++)
+    {
+      var row = reservedSeats[i][0];
+      var seat = reservedSeats[i][1];
+
+      if (!map.containsKey(row))
+        map.put(row, new HashSet<Integer>());
+
+      map.get(row).add(seat);
+    }
+
+    var ans = 0;
+    
+    for (Integer key : map.keySet()) {
+      ans += getAllocated(map.get(key));
+    }
+    
+    ans += (n - map.size()) * 2;
+
+    return ans;
+  }
+  
+  private int getAllocated(HashSet<Integer> seats) {
+    var leftIsle =
+      !seats.contains(2) &&
+      !seats.contains(3) &&
+      !seats.contains(4) &&
+      !seats.contains(5);
+
+    var rightIsle =
+      !seats.contains(6) &&
+      !seats.contains(7) &&
+      !seats.contains(8) &&
+      !seats.contains(9);
+
+    if (leftIsle && rightIsle)
+      return 2;
+
+    if (leftIsle || rightIsle)
+      return 1;
+
+    var center =
+      !seats.contains(4) &&
+      !seats.contains(5) &&
+      !seats.contains(6) &&
+      !seats.contains(7);
+
+    if (center)
+      return 1;
+
+    return 0;
+  }
+}
