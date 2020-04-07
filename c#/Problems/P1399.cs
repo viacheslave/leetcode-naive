8f99fcae1c5f7ed8e891eca2748a45c80ea3800e
Viacheslav Zholnovach <vyajol@gmail.com>
2020-04-07 22:33:20 +0300

    P1399

---
+++ c#/Problems/P1399.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/count-largest-group/
+	///		Submission: https://leetcode.com/submissions/detail/321330747/
+	/// </summary>
+	internal class P1399
+	{
+		public int CountLargestGroup(int n)
+		{
+			var digitSumToCount = Enumerable.Range(1, n)
+				.GroupBy(x => x.ToString().ToCharArray().Select(r => int.Parse(r.ToString())).Sum())
+				.OrderBy(x => x.Key)
+				.ToDictionary(x => x.Key, x => x.Count());
+
+			var max = digitSumToCount.Values.Max();
+
+			return digitSumToCount.Count(x => x.Value == max);
+		}
+	}
+}
