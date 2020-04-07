ca964e4455f806af0d1629b3b646e5630c4497f9
Viacheslav Zholnovach <vyajol@gmail.com>
2020-04-07 22:10:52 +0300

    P1395

---
+++ c#/Problems/P1395.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/count-number-of-teams/
+	///		Submission: https://leetcode.com/submissions/detail/321322737/
+	/// </summary>
+	internal class P1395
+	{
+		public int NumTeams(int[] rating)
+		{
+			return Find(rating) + Find(rating.Reverse().ToArray());
+		}
+
+		private int Find(int[] arr)
+		{
+			var ans = 0;
+
+			for (int i = 0; i < arr.Length - 2; i++)
+			{
+				for (int j = i + 1; j < arr.Length - 1; j++)
+				{
+					if (arr[j] < arr[i])
+						continue;
+
+					for (int k = j + 1; k < arr.Length; k++)
+					{
+						if (arr[k] < arr[j])
+							continue;
+
+						ans++;
+					}
+				}
+			}
+
+			return ans;
+		}
+	}
+}
