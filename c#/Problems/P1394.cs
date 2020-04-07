a205cb7da273e0d3ba267f63bd631d988d2d9c92
Viacheslav Zholnovach <vyajol@gmail.com>
2020-04-07 18:46:04 +0300

    P1394

---
+++ c#/Problems/P1394.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/find-lucky-integer-in-an-array/
+	///		Submission: https://leetcode.com/submissions/detail/321246653/
+	/// </summary>
+	internal class P1394
+	{
+		public int FindLucky(int[] arr)
+		{
+			var items = arr
+				.GroupBy(x => x)
+				.Where(x => x.Key == x.Count())
+				.OrderBy(x => x.Key)
+				.Select(x => x.Key)
+				.ToList();
+			return items.Count > 0
+				? items.Last()
+				: -1;
+		}
+	}
+}
