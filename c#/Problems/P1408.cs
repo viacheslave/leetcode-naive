48f69d2d88e2a6e7b653b5b0e7bce8b420aada4a
Viacheslav Zholnovach <vyajol@gmail.com>
2020-04-13 23:57:39 +0300

    P1408

---
+++ c#/Problems/P1408.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/string-matching-in-an-array/
+	///		Submission: https://leetcode.com/submissions/detail/324358094/
+	/// </summary>
+	internal class P1408
+	{
+		public IList<string> StringMatching(string[] words)
+		{
+			var ans = new List<string>();
+			words = words.OrderBy(w => w.Length).ToArray();
+
+			for (int i = 0; i < words.Length; i++)
+			{
+				for (int j = i + 1; j < words.Length; j++)
+				{
+					if (words[j].IndexOf(words[i]) != -1)
+					{
+						ans.Add(words[i]);
+						break;
+					}
+				}
+			}
+
+			return ans.ToList();
+		}
+	}
+}
