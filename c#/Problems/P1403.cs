0c1e3ef98c90914612f63d1817242cafcfe6caa5
Viacheslav Zholnovach <vyajol@gmail.com>
2020-04-14 00:03:49 +0300

    P1403

---
+++ c#/Problems/P1403.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/
+	///		Submission: https://leetcode.com/submissions/detail/324360579/
+	/// </summary>
+	internal class P1403
+	{
+		public IList<int> MinSubsequence(int[] nums)
+		{
+			Array.Sort(nums);
+
+			var ans = new List<int>();
+			var sum = nums.Sum();
+			var currentSum = 0;
+			var i = nums.Length - 1;
+
+			while (currentSum <= sum - currentSum)
+			{
+				ans.Add(nums[i]);
+				currentSum += nums[i];
+				i--;
+			}
+
+			return ans;
+		}
+	}
+}
