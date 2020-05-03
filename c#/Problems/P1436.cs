d03b0aca5b909d0caae9ac1b5175a34f3eb53715
Viacheslav Zholnovach <vyajol@gmail.com>
2020-05-03 16:19:28 +0300

    P1436

---
+++ c#/Problems/P1436.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/destination-city/
+	///		Submission: https://leetcode.com/submissions/detail/333774371/
+	/// </summary>
+	internal class P1436
+	{
+		public string DestCity(IList<IList<string>> paths)
+		{
+			var dests = paths.Select(p => p[1]).Distinct();
+			var starts = paths.Select(p => p[0]).Distinct();
+
+			return dests.First(value => !starts.Contains(value));
+		}
+	}
+}
