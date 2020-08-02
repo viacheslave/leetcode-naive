f11561952888621cd20d2a28d0d604f3222f705f
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-02 17:33:01 +0300

    P1497.java

---
+++ java/src/com/naive/leetcode/P1497.java
@@ -0,0 +1,44 @@
+package com.naive.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * Problem: https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/
+ * Submission: https://leetcode.com/submissions/detail/374961018/
+ */
+public class P1497 {
+    class Solution {
+        public boolean canArrange(int[] arr, int k) {
+            var map = new HashMap<Integer, Integer>();
+
+            for (var el: arr) {
+                var key = (el + k) % k;
+                if (el < 0)
+                    key = (k - ((k - el) % k)) % k;
+
+                map.putIfAbsent(key, 0);
+                map.put(key, map.get(key) + 1);
+            }
+
+            for (var key : map.keySet()) {
+                var otherkey = k - key;
+
+                if (key == 0 || key == otherkey)
+                {
+                    if (map.get(key) % 2 == 1)
+                        return false;
+                    else
+                        continue;
+                }
+
+                if (!map.containsKey(otherkey))
+                    return false;
+
+                if (map.get(key).intValue() != map.get(otherkey).intValue())
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
