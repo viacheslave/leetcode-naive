2b171cf51313325084c4aa520bf969e644e086f6
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-03 14:35:54 +0300

    formatting

--- c#/Problems/P0865.cs
+++ c#/Problems/P0865.cs
@@ -11,56 +11,56 @@ namespace LeetCode.Naive.Problems
 	/// </summary>
 	internal class P0865
 	{
-    private TreeNode _subTree = null;
+		private TreeNode _subTree = null;
 
-    public TreeNode SubtreeWithAllDeepest(TreeNode root)
-    {
-      var nodes = new List<(TreeNode, int)>();
-      TraverseDepths(0, root, nodes);
+		public TreeNode SubtreeWithAllDeepest(TreeNode root)
+		{
+			var nodes = new List<(TreeNode, int)>();
+			TraverseDepths(0, root, nodes);
 
-      var maxDepth = nodes.Max(x => x.Item2);
-      var maxDepthNodes = nodes.Where(x => x.Item2 == maxDepth).Select(x => x.Item1).Select(x => x.val).ToHashSet();
+			var maxDepth = nodes.Max(x => x.Item2);
+			var maxDepthNodes = nodes.Where(x => x.Item2 == maxDepth).Select(x => x.Item1).Select(x => x.val).ToHashSet();
 
-      TraverseNodes(root, maxDepthNodes);
+			TraverseNodes(root, maxDepthNodes);
 
-      return _subTree;
-    }
+			return _subTree;
+		}
 
-    private List<int> TraverseNodes(TreeNode node, HashSet<int> maxDepthNodes)
-    {
-      if (node == null)
-        return new List<int>();
+		private List<int> TraverseNodes(TreeNode node, HashSet<int> maxDepthNodes)
+		{
+			if (node == null)
+				return new List<int>();
 
-      if (_subTree != null)
-        return new List<int>();
+			if (_subTree != null)
+				return new List<int>();
 
-      var left = TraverseNodes(node.left, maxDepthNodes);
-      var right = TraverseNodes(node.right, maxDepthNodes);
+			var left = TraverseNodes(node.left, maxDepthNodes);
+			var right = TraverseNodes(node.right, maxDepthNodes);
 
-      var all = new List<int>();
-      all.AddRange(left);
-      all.AddRange(right);
-      all.Add(node.val);
+			var all = new List<int>();
+			all.AddRange(left);
+			all.AddRange(right);
+			all.Add(node.val);
 
-      if (maxDepthNodes.IsSubsetOf(all) && _subTree == null)
-      {
-        _subTree = node;
-        return all;
-      }
+			if (maxDepthNodes.IsSubsetOf(all) && _subTree == null)
+			{
+				_subTree = node;
+				return all;
+			}
 
 
-      return all;
-    }
+			return all;
+		}
 
-    private void TraverseDepths(int depth, TreeNode node, List<(TreeNode, int)> nodes)
-    {
-      if (node == null)
-        return;
+		private void TraverseDepths(int depth, TreeNode node, List<(TreeNode, int)> nodes)
+		{
+			if (node == null)
+				return;
 
-      nodes.Add((node, depth));
+			nodes.Add((node, depth));
 
-      TraverseDepths(depth + 1, node.left, nodes);
-      TraverseDepths(depth + 1, node.right, nodes);
-    }
-  }
+			TraverseDepths(depth + 1, node.left, nodes);
+			TraverseDepths(depth + 1, node.right, nodes);
+		}
+	}
 }
