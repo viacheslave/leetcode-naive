d2b605068366ac672e714b3e739e8c288db82352
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-09 12:09:15 +0300

    P1545.java

---
+++ java/src/com/naive/leetcode/P1545.java
@@ -0,0 +1,37 @@
+package com.naive.leetcode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Problem: https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/
+ * Submission: https://leetcode.com/submissions/detail/378325866/
+ */
+public class P1545 {
+    class Solution {
+        public char findKthBit(int n, int k) {
+            var Sn = BuildS(n);
+
+            return Sn[k - 1] == true ? '1' : '0';
+        }
+
+        private boolean[] BuildS(int n) {
+            if (n == 1) {
+                return new boolean[] {false};
+            }
+
+            var list = BuildS(n - 1);
+
+            var newList = new boolean[list.length * 2 + 1];
+
+            for (var i = 0; i < list.length; i++) {
+                newList[i] = list[i];
+                newList[newList.length - 1 - i] = !list[i];
+            }
+
+            newList[list.length] = true;
+            return newList;
+        }
+    }
+}
