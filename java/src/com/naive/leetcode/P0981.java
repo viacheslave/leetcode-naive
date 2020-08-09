d91450b7914ff0aa4e60ec5f6e927b973ef09143
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-09 23:50:00 +0300

    P0981.java

---
+++ java/src/com/naive/leetcode/P0981.java
@@ -0,0 +1,44 @@
+package com.naive.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Problem: https://leetcode.com/problems/time-based-key-value-store/
+ * Submission: https://leetcode.com/submissions/detail/378546627/
+ */
+public class P0981 {
+    class TimeMap {
+
+        private HashMap<String, TreeMap<Integer, String>> map = new HashMap<>();
+
+        /** Initialize your data structure here. */
+        public TimeMap() {
+
+        }
+
+        public void set(String key, String value, int timestamp) {
+            map.putIfAbsent(key, new TreeMap<>());
+            map.get(key).put(timestamp, value);
+        }
+
+        public String get(String key, int timestamp) {
+            if (!map.containsKey(key))
+                return "";
+
+            Map.Entry<Integer, String> entry = map.get(key).floorEntry(timestamp);
+            if (entry == null)
+                return "";
+
+            return entry.getValue();
+        }
+    }
+
+    /**
+     * Your TimeMap object will be instantiated and called as such:
+     * TimeMap obj = new TimeMap();
+     * obj.set(key,value,timestamp);
+     * String param_2 = obj.get(key,timestamp);
+     */
+}
