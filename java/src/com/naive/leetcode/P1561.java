8538d5ee8cd6922b8b76c58088e6a2a7c5952701
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-24 20:44:06 +0300

    P1561.java

---
+++ java/src/com/naive/leetcode/P1561.java
@@ -0,0 +1,25 @@
+package com.naive.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Problem: https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+ * Submission: https://leetcode.com/submissions/detail/385750904/
+ */
+public class P1561 {
+    class Solution {
+        public int maxCoins(int[] piles) {
+            Arrays.sort(piles);
+
+            var ans = 0;
+
+            for (var i = 0; i < piles.length / 3; i++) {
+                var index = piles.length - 2 - 2 * i;
+                ans += piles[index];
+            }
+
+            return ans;
+        }
+    }
+}
