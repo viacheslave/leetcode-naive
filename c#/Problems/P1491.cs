2338f5b871c0dc644c1126a7f9cd574add583045
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-27 18:09:49 +0300

    P1491

---
+++ c#/Problems/P1491.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+	///		Submission: https://leetcode.com/submissions/detail/387147571/
+	/// </summary>
+	internal class P1491
+	{
+		public double Average(int[] salary)
+		{
+			return salary.OrderBy(c => c).Skip(1).Take(salary.Length - 2).Average();
+		}
+	}
+}
