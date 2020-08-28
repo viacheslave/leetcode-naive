3d84382bc57a6f95ad75b01e80046a92806335f4
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-28 21:29:54 +0300

    P1550

---
+++ c#/Problems/P1550.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/three-consecutive-odds/
+	///		Submission: https://leetcode.com/submissions/detail/387676050/
+	/// </summary>
+	internal class P1550
+	{
+		public bool ThreeConsecutiveOdds(int[] arr)
+		{
+			if (arr.Length < 3)
+				return false;
+
+			for (var i = 0; i < arr.Length - 2; i++)
+				if ((arr[i] % 2) == 1 && (arr[i + 1] % 2) == 1 && (arr[i + 2] % 2) == 1)
+					return true;
+
+			return false;
+		}
+	}
+}
