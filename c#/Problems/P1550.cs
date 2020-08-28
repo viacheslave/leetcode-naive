674a424363bbf5b03e0cfeab29d718b8de841377
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-28 22:33:02 +0300

    P1450

--- c#/Problems/P1550.cs
+++ c#/Problems/P1550.cs
@@ -22,4 +22,23 @@ namespace LeetCode.Naive.Problems
 			return false;
 		}
 	}
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+	///		Submission: https://leetcode.com/submissions/detail/387698346/
+	/// </summary>
+	internal class P1450
+	{
+		public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+		{
+			var ans = 0;
+
+			for (var i = 0; i < startTime.Length; i++)
+			{
+				if (startTime[i] <= queryTime && queryTime <= endTime[i])
+					ans++;
+			}
+
+			return ans;
+		}
+	}
 }
