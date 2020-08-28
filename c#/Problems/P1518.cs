fe120b19722979760e5f97cd76fa58b129780c70
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-28 22:09:02 +0300

    P1518

---
+++ c#/Problems/P1518.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/water-bottles/
+	///		Submission: https://leetcode.com/submissions/detail/387690183/
+	/// </summary>
+	internal class P1518
+	{
+		public int NumWaterBottles(int numBottles, int numExchange)
+		{
+			var ans = 0;
+
+			while (numBottles >= numExchange)
+			{
+				ans += numExchange;
+				numBottles -= numExchange;
+				numBottles++;
+			}
+
+			return ans + numBottles;
+		}
+	}
+}
