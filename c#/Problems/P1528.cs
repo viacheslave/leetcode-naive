1af2ee37c22bf11ad93243ee9826ee33ae1c841f
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-28 21:57:53 +0300

    P1528

---
+++ c#/Problems/P1528.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/shuffle-string/
+	///		Submission: https://leetcode.com/submissions/detail/387686162/
+	/// </summary>
+	internal class P1528
+	{
+		public string RestoreString(string s, int[] indices)
+		{
+			var ans = new char[s.Length];
+
+			for (var i = 0; i < s.Length; i++)
+				ans[indices[i]] = s[i];
+
+			return new string(ans);
+		}
+	}
+}
