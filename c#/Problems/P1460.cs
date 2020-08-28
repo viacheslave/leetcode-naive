e040b9e069132e5f8dc1700d1b9bc55e423d43b1
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-28 23:04:03 +0300

    P1460

---
+++ c#/Problems/P1460.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/make-two-arrays-equal-by-reversing-sub-arrays/
+	///		Submission: https://leetcode.com/submissions/detail/387708790/
+	/// </summary>
+	internal class P1460
+	{
+		public bool CanBeEqual(int[] target, int[] arr)
+		{
+			Array.Sort(target);
+			Array.Sort(arr);
+
+			for (var i = 0; i < target.Length; i++)
+				if (target[i] != arr[i])
+					return false;
+
+			return true;
+		}
+	}
+}
