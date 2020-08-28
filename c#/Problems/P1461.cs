304df1fde320b02cef52f614dc45f0502fa97072
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-29 00:17:59 +0300

    P1461

---
+++ c#/Problems/P1461.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+	///		Submission: https://leetcode.com/submissions/detail/387732235/
+	/// </summary>
+	internal class P1461
+	{
+		public bool HasAllCodes(string s, int k)
+		{
+			var codes = new HashSet<string>();
+
+			for (int i = 0; i < s.Length - k + 1; i++)
+			{
+				var code = s.Substring(i, k);
+				if (!codes.Contains(code))
+					codes.Add(code);
+			}
+
+			return codes.Count == Math.Pow(2, k);
+		}
+	}
+}
