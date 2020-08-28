97efb07b3fa3d2ce82047276ea76d464269894e4
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-28 22:21:32 +0300

    P1475

---
+++ c#/Problems/P1475.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+	///		Submission: https://leetcode.com/submissions/detail/387694419/
+	/// </summary>
+	internal class P1475
+	{
+		public int[] FinalPrices(int[] prices)
+		{
+			var ans = new int[prices.Length];
+
+			for (var i = 0; i < prices.Length; i++)
+			{
+				ans[i] = prices[i];
+
+				for (var j = i + 1; j < prices.Length; j++)
+				{
+					if (prices[j] <= prices[i])
+					{
+						ans[i] = prices[i] - prices[j];
+						break;
+					}
+				}
+			}
+
+
+			return ans;
+		}
+	}
+}
