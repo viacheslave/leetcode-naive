e544c90a4203db0325154c0484528a5702fcee43
Viacheslav Zholnovach <vyajol@gmail.com>
2020-08-28 22:14:31 +0300

    P1523

---
+++ c#/Problems/P1523.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+	///		Submission: https://leetcode.com/submissions/detail/387692026/
+	/// </summary>
+	internal class P1523
+	{
+		public int CountOdds(int low, int high)
+		{
+			var lowodd = low % 2 == 1;
+			var highodd = high % 2 == 1;
+
+			if (lowodd && highodd)
+				return (high - low) / 2 + 1;
+
+			if (!lowodd && !highodd)
+				return (high - low) / 2;
+
+			return (high - low + 1) / 2;
+		}
+	}
+}
