9f6ab510d6560f993012da5c775a6f13bb1a84b5
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-02 19:58:25 +0300

    P1566

---
+++ c#/Problems/P1566.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/detect-pattern-of-length-m-repeated-k-or-more-times/
+	///		Submission: https://leetcode.com/submissions/detail/390029009/
+	/// </summary>
+	internal class P1566
+	{
+		public bool ContainsPattern(int[] arr, int m, int k)
+		{
+			for (var startIndex = 0; startIndex <= arr.Length - m * k; startIndex++)
+			{
+				var contains = true;
+
+				for (var pos = 0; pos < m; pos++)
+				{
+					contains &= Repeats(arr, startIndex + pos, m, k);
+				}
+
+				if (contains)
+					return true;
+			}
+
+			return false;
+		}
+
+		private bool Repeats(int[] arr, int pos, int m, int k)
+		{
+			var values = new int[k];
+
+			for (var i = 0; i < k; i++)
+			{
+				values[i] = arr[pos + m * i];
+			}
+
+			return values.Distinct().Count() == 1;
+		}
+	}
+}
