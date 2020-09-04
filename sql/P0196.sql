d1727007d29be2e7395bf256a004a6e629c0e50a
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-04 21:42:56 +0300

    P0196.sql

---
+++ sql/P0196.sql
@@ -0,0 +1,16 @@
+﻿/*
+Problem: https://leetcode.com/problems/delete-duplicate-emails/
+Submission: https://leetcode.com/submissions/detail/391020942/
+*/
+
+# Write your MySQL query statement below
+delete from Person
+where Id not in (
+  select x.Id from
+  (
+    select min(Id) as Id 
+    from Person
+    group by Email
+  ) as x
+)
+group by id
