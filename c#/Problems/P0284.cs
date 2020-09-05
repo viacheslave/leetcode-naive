b6f4585b6442851ad87a72846f5be5079e19cbaf
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-05 15:09:14 +0300

    P0284

---
+++ c#/Problems/P0284.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/peeking-iterator/
+	///		Submission: https://leetcode.com/submissions/detail/391302022/
+	/// </summary>
+	internal class P0284
+	{
+		// C# IEnumerator interface reference:
+		// https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerator?view=netframework-4.8
+
+		class PeekingIterator
+		{
+			private int? _next = null;
+
+			private readonly IEnumerator<int> _iterator;
+
+			// iterators refers to the first element of the array.
+			public PeekingIterator(IEnumerator<int> iterator)
+			{
+				_iterator = iterator;
+
+				_next = _iterator.Current;
+			}
+
+			// Returns the next element in the iteration without advancing the iterator.
+			public int Peek()
+			{
+				return _next.GetValueOrDefault();
+			}
+
+			// Returns the next element in the iteration and advances the iterator.
+			public int Next()
+			{
+				var value = _next.GetValueOrDefault();
+
+				_next = _iterator.MoveNext()
+					? _iterator.Current
+					: (int?)null;
+
+				return value;
+			}
+
+			// Returns false if the iterator is refering to the end of the array of true otherwise.
+			public bool HasNext()
+			{
+				return _next.HasValue;
+			}
+		}
+	}
+}
