0876beca12f6f0b6f5b1209e2a2d3688c7c80ca9
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-06 21:44:55 +0300

    P1572

---
+++ c#/Problems/P1572.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/matrix-diagonal-sum/
+	///		Submission: https://leetcode.com/submissions/detail/391937433/
+	/// </summary>
+	internal class P1572
+	{
+		public int DiagonalSum(int[][] mat)
+		{
+			var ans = 0;
+
+			for (var i = 0; i < mat.Length; i++)
+			{
+				ans += mat[i][mat.Length - 1 - i];
+				ans += mat[i][i];
+
+				if (i == mat.Length - 1 - i)
+					ans -= mat[i][i];
+			}
+
+			return ans;
+		}
+	}
+}
