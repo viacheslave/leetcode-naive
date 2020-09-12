144e8d7d5575275a4d330c9cff08e6f7084b4e9f
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-12 19:59:13 +0300

    P0318

---
+++ c#/Problems/P0318.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/maximum-product-of-word-lengths/
+	///		Submission: https://leetcode.com/submissions/detail/394667554/
+	/// </summary>
+	internal class P0318
+	{
+		public int MaxProduct(string[] words)
+		{
+			var map = new Dictionary<string, int>();
+			foreach (var word in words)
+			{
+				var value = 0;
+				foreach (var ch in word.Distinct().ToHashSet())
+					value += (int)Math.Pow(2, ch - 97);
+
+				map[word] = value;
+			}
+
+			var values = map.ToList();
+			var max = 0;
+
+			for (var i = 0; i < values.Count; i++)
+				for (var j = 1; j < values.Count; j++)
+					if ((values[i].Value & values[j].Value) == 0)
+						max = Math.Max(max, values[i].Key.Length * values[j].Key.Length);
+
+			return max;
+		}
+	}
+}
