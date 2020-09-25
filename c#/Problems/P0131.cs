b924700a9c7fd29b9f630ee54bebc07415252cac
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-25 20:26:30 +0300

    P0131

---
+++ c#/Problems/P0131.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/palindrome-partitioning/
+	///		Submission: https://leetcode.com/submissions/detail/400631484/
+	/// </summary>
+	internal class P0131
+	{
+    public IList<IList<string>> Partition(string s)
+    {
+      var ans = new List<IList<string>>();
+      var current = new List<string>();
+
+      Partition(0, s, current, ans);
+
+      return ans;
+    }
+
+    private void Partition(int start, string s, IList<string> current, List<IList<string>> ans)
+    {
+      if (start == s.Length)
+      {
+        ans.Add(new List<string>(current));
+        return;
+      }
+
+      for (var end = start; end < s.Length; end++)
+      {
+        if (Palindrome(s, start, end))
+        {
+          current.Add(s.Substring(start, end - start + 1));
+
+          Partition(end + 1, s, current, ans);
+
+          current.RemoveAt(current.Count - 1);
+        }
+      }
+    }
+
+    private bool Palindrome(string s, int start, int end)
+    {
+      for (var i = 0; i < (end - start + 1) / 2; i++)
+        if (s[start + i] != s[end - i])
+          return false;
+
+      return true;
+    }
+  }
+}
