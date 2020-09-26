a1336151bb6c7c0f7d2dd05b01d0ff986febaf20
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-26 17:53:49 +0300

    P0790

---
+++ c#/Problems/P0790.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/domino-and-tromino-tiling/
+	///		Submission: https://leetcode.com/submissions/detail/400968207/
+	/// </summary>
+	internal class P0790
+	{
+		public int NumTilings(int N)
+		{
+			var dp = new Dictionary<int, int>
+			{
+				[0] = 1,
+				[1] = 1
+			};
+
+			var mod = 1_000_000_007;
+
+			for (var i = 2; i <= N; i++)
+			{
+				dp[i] = 0;
+
+				for (var j = 1; i - j >= 0; j++)
+				{
+					if (j <= 2)
+					{
+						dp[i] += dp[i - j];
+						dp[i] %= mod;
+					}
+					else
+					{
+						dp[i] += dp[i - j];
+						dp[i] %= mod;
+
+						dp[i] += dp[i - j];
+						dp[i] %= mod;
+					}
+				}
+			}
+
+			return dp[N];
+		}
+	}
+}
