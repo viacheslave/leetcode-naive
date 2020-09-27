a733e97a9677ad9032f3f5316b802daecfa60e98
Viacheslav Zholnovach <vyajol@gmail.com>
2020-09-27 11:04:15 +0300

    P1598

---
+++ c#/Problems/P1598.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/crawler-log-folder/
+	///		Submission: https://leetcode.com/submissions/detail/401316170/
+	/// </summary>
+	internal class P1598
+	{
+		public int MinOperations(string[] logs)
+		{
+			var st = new Stack<string>();
+
+			foreach (var log in logs)
+			{
+				if (log == "./")
+					continue;
+
+				if (log == "../")
+				{
+					if (st.Count > 0)
+						st.Pop();
+					continue;
+				}
+
+				st.Push(log);
+			}
+
+			return st.Count;
+		}
+	}
+}
