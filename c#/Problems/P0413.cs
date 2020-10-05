933bc23e12ab47d8c4c62c060bc7c88078af3e89
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-05 21:11:49 +0300

    P0413

--- c#/Problems/P0413.cs
+++ c#/Problems/P0413.cs
@@ -1,79 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 
 namespace LeetCode.Naive.Problems
 {
 	/// <summary>
-	///		Problem: https://leetcode.com/problems/third-maximum-number/
-	///		Submission: https://leetcode.com/submissions/detail/229966588/
+	///		Problem: https://leetcode.com/problems/arithmetic-slices/
+	///		Submission: https://leetcode.com/submissions/detail/404911996/
 	/// </summary>
 	internal class P0413
 	{
-		public int ThirdMax(int[] nums)
+		public int NumberOfArithmeticSlices(int[] A)
 		{
-			if (nums.Length == 0)
-				return 0;
+			var dp = Enumerable.Range(1, A.Length)
+					.Select(x => new Dictionary<int, int>())
+					.ToList();
 
-			if (nums.Length == 1)
-				return nums[0];
-
-			var maxArray = new List<int>();
-
-			for (var i = 0; i < nums.Length; i++)
+			for (var i = 2; i < A.Length; i++)
 			{
-				maxArray.Add(nums[i]);
+				var res = new Dictionary<int, int>();
 
-				Shift(maxArray);
-			}
-
-			return GetThirdMax(maxArray);
-		}
-
-		private void Shift(List<int> arr)
-		{
-			if (arr.Count == 1)
-				return;
-
-			int index = arr.Count - 1;
-			while (index > 0)
-			{
-				if (arr[index] > arr[index - 1])
+				foreach (var entry in dp[i - 1])
 				{
-					var tmp = arr[index];
-					arr[index] = arr[index - 1];
-					arr[index - 1] = tmp;
-
-					index--;
-					continue;
+					if (A[i] - A[i - 1] == entry.Key)
+						res[entry.Key] = entry.Value;
 				}
-				break;
-			}
-		}
 
-		private int GetThirdMax(List<int> arr)
-		{
-			int ticks = 1;
-			int max = arr[0];
-
-			foreach (var a in arr)
-			{
-				if (a == max)
-					continue;
-
-				if (a < max)
+				if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
 				{
-					max = a;
-					ticks++;
+					var key = A[i] - A[i - 1];
 
-					if (ticks == 3)
-						return max;
+					if (!res.ContainsKey(key))
+						res.Add(key, 0);
+
+					res[key]++;
 				}
+
+				dp[i] = res;
 			}
 
-			return arr[0];
+			return dp.Sum(x => x.Sum(f => f.Value));
 		}
 	}
 }
---
+++ c#/Problems/P0414.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/third-maximum-number/
+	///		Submission: https://leetcode.com/submissions/detail/229966588/
+	/// </summary>
+	internal class P0414
+	{
+		public int ThirdMax(int[] nums)
+		{
+			if (nums.Length == 0)
+				return 0;
+
+			if (nums.Length == 1)
+				return nums[0];
+
+			var maxArray = new List<int>();
+
+			for (var i = 0; i < nums.Length; i++)
+			{
+				maxArray.Add(nums[i]);
+
+				Shift(maxArray);
+			}
+
+			return GetThirdMax(maxArray);
+		}
+
+		private void Shift(List<int> arr)
+		{
+			if (arr.Count == 1)
+				return;
+
+			int index = arr.Count - 1;
+			while (index > 0)
+			{
+				if (arr[index] > arr[index - 1])
+				{
+					var tmp = arr[index];
+					arr[index] = arr[index - 1];
+					arr[index - 1] = tmp;
+
+					index--;
+					continue;
+				}
+				break;
+			}
+		}
+
+		private int GetThirdMax(List<int> arr)
+		{
+			int ticks = 1;
+			int max = arr[0];
+
+			foreach (var a in arr)
+			{
+				if (a == max)
+					continue;
+
+				if (a < max)
+				{
+					max = a;
+					ticks++;
+
+					if (ticks == 3)
+						return max;
+				}
+			}
+
+			return arr[0];
+		}
+	}
+}
