b85e31a62c9c2bfdec17e4cbb8a4ba693d507770
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-10 17:10:59 +0300

    P0452

---
+++ c#/Problems/P0452.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+	///		Submission: https://leetcode.com/submissions/detail/406930851/
+	/// </summary>
+	internal class P0452
+	{
+		public int FindMinArrowShots(int[][] points)
+		{
+			if (points.Length == 0)
+				return 0;
+
+			var ps = points.OrderBy(x => x[1])
+					.Select(x => (start: x[0], end: x[1]))
+					.ToList();
+
+			var ans = 1;
+
+			var pointer = ps[0].end;
+
+			for (var i = 0; i < ps.Count; i++)
+			{
+				var point = ps[i];
+				if (point.start <= pointer)
+					continue;
+
+				ans++;
+				pointer = point.end;
+			}
+
+			return ans;
+		}
+	}
+}
