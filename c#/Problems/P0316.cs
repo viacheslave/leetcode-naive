b4e94e28a811fb80a743eb3c2137c6e38f2bcc80
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-11 15:03:50 +0300

    P0316, P1081

---
+++ c#/Problems/P0316.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/remove-duplicate-letters/
+	///		Submission: https://leetcode.com/problems/remove-duplicate-letters/submissions/
+	/// </summary>
+	internal class P0316
+	{
+		public string RemoveDuplicateLetters(string s)
+		{
+			var chMap = s.GroupBy(c => c)
+					.ToDictionary(c => c.Key, c => c.Count());
+
+			var processed = s.Distinct()
+					.ToDictionary(c => c, _ => false);
+
+			var sb = new StringBuilder();
+
+			for (var i = 0; i < s.Length; i++)
+			{
+				var ch = s[i];
+
+				chMap[ch]--;
+
+				if (processed[ch])
+					continue;
+
+				while (sb.Length > 0)
+				{
+					var last = sb[^1];
+
+					if (last > ch && chMap[last] > 0)
+					{
+						processed[last] = false;
+						sb.Remove(sb.Length - 1, 1);
+
+						continue;
+					}
+
+					break;
+				}
+
+				processed[ch] = true;
+				sb.Append(ch);
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ c#/Problems/P1081.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+	///		Submission: https://leetcode.com/submissions/detail/407332555/
+	/// </summary>
+	internal class P1081
+	{
+		public IList<int> PeopleIndexes(IList<IList<string>> favoriteCompanies)
+		{
+			var fc = favoriteCompanies
+				.Select((c, i) => (i, c.ToHashSet()))
+				.OrderBy(c => c.Item2.Count)
+				.ToList();
+
+			var ans = new HashSet<int>();
+
+			for (int i = 0; i < fc.Count; i++)
+			{
+				for (int j = i + 1; j < fc.Count; j++)
+				{
+					if (fc[i].Item2.IsSubsetOf(fc[j].Item2))
+					{
+						ans.Add(fc[i].i);
+						break;
+					}
+				}
+			}
+
+			return Enumerable.Range(0, fc.Count)
+				.Where(c => !ans.Contains(c))
+				.OrderBy(x => x)
+				.ToList();
+		}
+	}
+}
