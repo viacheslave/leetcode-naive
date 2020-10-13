d471c13d98dc59915d30efaa4d8e0383a3fd2d3a
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-13 13:18:38 +0300

    P0287 (not really)

---
+++ c#/Problems/P0287.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+	/// <summary>
+	///		Problem: https://leetcode.com/problems/find-the-duplicate-number/
+	///		Submission: https://leetcode.com/submissions/detail/407761600/
+	/// </summary>
+	internal class P0287
+	{
+		public int FindDuplicate(int[] nums)
+		{
+			Array.Sort(nums);
+
+			var sum = 0;
+			for (var i = 0; i < nums.Length - 1; i++)
+				if (nums[i] == nums[i + 1])
+					return nums[i];
+
+			return -1;
+		}
+	}
+}
