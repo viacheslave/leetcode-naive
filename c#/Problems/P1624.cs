b500943d63fbbaf216bde7f15115984b61402767
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-18 18:24:12 +0300

    P1624

---
+++ c#/Problems/P1624.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///   Problem: https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+  ///   Submission: https://leetcode.com/submissions/detail/410266192/
+  /// </summary>
+  internal class P1624
+  {
+    public int MaxLengthBetweenEqualCharacters(string s)
+    {
+      var multiple = s.GroupBy(ch => ch)
+        .Where(c => c.Count() > 1)
+        .ToDictionary(c => c.Key, c => c.Count());
+
+      var maxDistance = int.MinValue;
+
+      foreach (var entry in multiple)
+      {
+        var first = s.IndexOf(entry.Key);
+        var last = s.LastIndexOf(entry.Key);
+
+        var distance = last - first - 1;
+        maxDistance = Math.Max(maxDistance, distance);
+      }
+
+      return maxDistance == int.MinValue ? -1 : maxDistance;
+    }
+  }
+}
