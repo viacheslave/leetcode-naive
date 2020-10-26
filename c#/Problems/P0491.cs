6bfe7836ec039d3a8d07229e52b51a6a8177b95a
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-26 18:15:56 +0200

    P0491

---
+++ c#/Problems/P0491.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/increasing-subsequences/
+  ///    Submission: https://leetcode.com/submissions/detail/413407624/
+  /// </summary>
+  internal class P0491
+  {
+    public class Solution
+    {
+      public IList<IList<int>> FindSubsequences(int[] nums)
+      {
+        var ans = new List<IList<int>>();
+
+        Traverse(nums, 0, new List<int>(), ans);
+
+        return ans;
+      }
+
+      private void Traverse(int[] nums, int start, List<int> list, List<IList<int>> ans)
+      {
+        if (list.Count > 1)
+          ans.Add(new List<int>(list));
+
+        var set = new HashSet<int>();
+
+        for (int i = start; i < nums.Length; i++)
+        {
+          if (list.Count > 0 && nums[i] < list[list.Count - 1])
+            continue;
+
+          if (set.Contains(nums[i]))
+            continue;
+
+          list.Add(nums[i]);
+
+          Traverse(nums, i + 1, list, ans);
+
+          list.RemoveAt(list.Count - 1);
+          set.Add(nums[i]);
+        }
+      }
+    }
+  }
+}
