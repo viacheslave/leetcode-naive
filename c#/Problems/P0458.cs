362b239b1ce69b759faef1fbf653f5376b269a07
Viacheslav Zholnovach <vyajol@gmail.com>
2020-11-14 13:59:18 +0200

    P0458

---
+++ c#/Problems/P0458.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/poor-pigs/
+  ///    Submission: https://leetcode.com/submissions/detail/420153278/
+  /// </summary>
+  internal class P0458
+  {
+    public class Solution
+    {
+      public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+      {
+        var t = minutesToTest / minutesToDie + 1;
+
+        var exp = 0;
+        for (; ; )
+        {
+          var pow = (int)Math.Pow(t, exp);
+          if (pow >= buckets)
+            return exp;
+
+          exp++;
+        }
+      }
+    }
+  }
+}
