8f38aceb19b0450ede0e3b64cfaea62c529d2b3e
Viacheslav Zholnovach <vyajol@gmail.com>
2020-11-18 11:20:29 +0200

    P0898

---
+++ c#/Problems/P0898.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/bitwise-ors-of-subarrays/
+  ///    Submission: https://leetcode.com/submissions/detail/421594160/
+  /// </summary>
+  internal class P0898
+  {
+    public class Solution
+    {
+      public int SubarrayBitwiseORs(int[] A)
+      {
+        var dp = new HashSet<int>[A.Length];
+        dp[0] = new HashSet<int>() { A[0] };
+
+        for (var i = 1; i < A.Length; ++i)
+        {
+          dp[i] = new HashSet<int>() { A[i] };
+
+          foreach (var entry in dp[i - 1])
+            dp[i].Add(entry | A[i]);
+        }
+
+        return dp.SelectMany(d => d).Distinct().Count();
+      }
+    }
+  }
+}
