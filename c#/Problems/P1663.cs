391e6a7761acfe5bef41ec19006d7453772f43ff
Viacheslav Zholnovach <vyajol@gmail.com>
2020-11-22 12:53:18 +0200

    P1663

---
+++ c#/Problems/P1663.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/
+  ///    Submission: https://leetcode.com/submissions/detail/422929286/
+  /// </summary>
+  internal class P1663
+  {
+    public class Solution
+    {
+      public string GetSmallestString(int n, int k)
+      {
+        var ans = new List<int>();
+
+        while (n > 0)
+        {
+          var max = Math.Min(k - (n - 1), 26);
+          ans.Add(max);
+
+          k -= max;
+          n--;
+        }
+
+        ans.Reverse();
+
+        return new string(ans.Select(i => (char)(97 - 1 + i)).ToArray());
+      }
+    }
+  }
+}
