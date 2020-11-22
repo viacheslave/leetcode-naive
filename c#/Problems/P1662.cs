79f8b24fe2169e41ac48d0365cb10bb9fe9ea803
Viacheslav Zholnovach <vyajol@gmail.com>
2020-11-22 12:59:04 +0200

    P1662

---
+++ c#/Problems/P1662.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+  ///    Submission: https://leetcode.com/submissions/detail/422930331/
+  /// </summary>
+  internal class P1662
+  {
+    public class Solution
+    {
+      public bool ArrayStringsAreEqual(string[] word1, string[] word2) =>
+        word1.Aggregate(new StringBuilder(), (sb, w) => sb.Append(w)).ToString() ==
+        word2.Aggregate(new StringBuilder(), (sb, w) => sb.Append(w)).ToString();
+    }
+  }
+}
