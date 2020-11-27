954fe4b1d5186b192fc8ac9cda9b8418db5cc5a8
Viacheslav Zholnovach <vyajol@gmail.com>
2020-11-27 15:31:50 +0200

    P0829

---
+++ c#/Problems/P0829.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/consecutive-numbers-sum/
+  ///    Submission: https://leetcode.com/submissions/detail/424638530/
+  /// </summary>
+  internal class P0829
+  {
+    public class Solution
+    {
+      public int ConsecutiveNumbersSum(int N)
+      {
+        var sqrt = Math.Sqrt(N);
+
+        var ans = 1;
+        for (var i = 2; i <= sqrt; i++)
+        {
+          if (N % i == 0)
+          {
+            if (i % 2 == 1) ans++;
+            if (i != (N / i) && (N / i) % 2 == 1) ans++;
+          }
+        }
+
+        if (N > 1 && N % 2 == 1)
+          ans++;
+
+        return ans;
+      }
+    }
+  }
+}
