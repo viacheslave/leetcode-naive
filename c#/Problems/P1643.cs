5dfe7449b9f79087d46e9d458848afa0bfb4fe71
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-06 11:17:06 +0200

    P1643, P1679

---
+++ c#/Problems/P1643.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/kth-smallest-instructions/
+  ///    Submission: https://leetcode.com/submissions/detail/427762202/
+  /// </summary>
+  internal class P1643
+  {
+    public class Solution
+    {
+      public string KthSmallestPath(int[] destination, int k)
+      {
+        var maxX = destination[0];
+        var maxY = destination[1];
+
+        var point = (x: 0, y: 0);
+        var target = (x: destination[0], y: destination[1]);
+        var range = (start: 1, end: Cr(maxX + maxY, maxX));
+
+        var sb = new StringBuilder();
+
+        while (point != target)
+        {
+          if (point.x == target.x)
+          {
+            point = (point.x, point.y + 1);
+            sb.Append("H");
+            continue;
+          }
+
+          if (point.y == target.y)
+          {
+            point = (point.x + 1, point.y);
+            sb.Append("V");
+            continue;
+          }
+
+          var lpoint = (x: point.x, y: point.y + 1);
+          var dpoint = (x: point.x + 1, y: point.y);
+
+          var lCr = Cr(maxY - lpoint.y + maxX - lpoint.x, maxX - lpoint.x);
+          var dCr = Cr(maxY - dpoint.y + maxX - dpoint.x, maxX - dpoint.x);
+
+          var hRange = (start: range.start, end: range.start + lCr - 1);
+          var vRange = (start: range.end - dCr + 1, end: range.end);
+
+          if (hRange.start <= k && k <= hRange.end)
+          {
+            range = hRange;
+            point = (point.x, point.y + 1);
+            sb.Append("H");
+          }
+          else
+          {
+            range = vRange;
+            point = (point.x + 1, point.y);
+            sb.Append("V");
+          }
+        }
+
+        return sb.ToString();
+      }
+
+      public int Cr(int n, int r)
+        => (int)(Fact(n) / (Fact(r) * Fact(n - r)));
+
+      public BigInteger Fact(int n)
+        => Enumerable.Range(1, n).Aggregate(BigInteger.One, (p, item) => BigInteger.Multiply(p, new BigInteger(item)));
+    }
+  }
+}
---
+++ c#/Problems/P1679.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/max-number-of-k-sum-pairs/
+  ///    Submission: https://leetcode.com/submissions/detail/427769021/
+  /// </summary>
+  internal class P1679
+  {
+    public class Solution
+    {
+      public int MaxOperations(int[] nums, int k)
+      {
+        var map = nums.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var visited = new HashSet<int>();
+        var ans = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+          if (visited.Contains(nums[i]))
+            continue;
+
+          if (nums[i] + nums[i] == k)
+          {
+            ans += map[nums[i]] / 2;
+            visited.Add(nums[i]);
+            continue;
+          }
+
+          ans += Math.Min(map[nums[i]],
+            map.ContainsKey(k - nums[i]) ? map[k - nums[i]] : 0);
+
+          visited.Add(nums[i]);
+          visited.Add(k - nums[i]);
+        }
+
+        return ans;
+      }
+    }
+  }
+}
