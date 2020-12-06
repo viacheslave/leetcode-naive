6521e5a2e9c7a589a1b712f697ff420e57d65a9a
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-06 15:38:24 +0200

    P1673

---
+++ c#/Problems/P1673.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/find-the-most-competitive-subsequence/
+  ///    Submission: https://leetcode.com/submissions/detail/427815957/
+  /// </summary>
+  internal class P1673
+  {
+    public class Solution
+    {
+      public int[] MostCompetitive(int[] nums, int k)
+      {
+        var stack = new Stack<int>();
+
+        for (var index = 0; index < nums.Length; index++)
+        {
+          var el = nums[index];
+          var available = nums.Length - index;
+
+          if (stack.Count == 0 || (stack.Count < k && stack.Peek() < el))
+          {
+            stack.Push(el);
+            continue;
+          }
+
+          while (stack.Count > 0 && stack.Peek() > el && stack.Count + available > k)
+            stack.Pop();
+
+          if (stack.Count < k)
+            stack.Push(el);
+        }
+
+        var list = stack.ToList();
+        list.Reverse();
+
+        return list.ToArray();
+      }
+    }
+  }
+}
