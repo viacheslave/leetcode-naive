5161ac4fa2db9a932f9412122dcd3391c5235f8f
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-06 11:27:52 +0200

    P1678

---
+++ c#/Problems/P1678.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/goal-parser-interpretation/
+  ///    Submission: https://leetcode.com/submissions/detail/427771126/
+  /// </summary>
+  internal class P1678
+  {
+    public class Solution
+    {
+      public string Interpret(string command)
+      {
+        var sb = new StringBuilder();
+        var index = 0;
+
+        while (index < command.Length)
+        {
+          if (command[index] == 'G')
+          {
+            sb.Append("G");
+            index++;
+            continue;
+          }
+
+          if (command.Substring(index, 2) == "()")
+          {
+            sb.Append("o");
+            index += 2;
+            continue;
+          }
+
+          sb.Append("al");
+          index += 4;
+        }
+
+        return sb.ToString();
+      }
+    }
+  }
+}
