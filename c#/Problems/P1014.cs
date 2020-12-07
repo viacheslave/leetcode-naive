cbc19ec26b2775df8a3d85ff78961290bf6fa323
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-07 16:43:40 +0200

    P1041

---
+++ c#/Problems/P1014.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/best-sightseeing-pair/
+  ///    Submission: https://leetcode.com/submissions/detail/428200063/
+  /// </summary>
+  internal class P1014
+  {
+    public class Solution
+    {
+      public int MaxScoreSightseeingPair(int[] A)
+      {
+        var left = 0;
+        var arr = new List<(int left, int right)>();
+
+        for (var i = 1; i < A.Length; i++)
+        {
+          left = Math.Max(left, A[i - 1] + (i - 1));
+          arr.Add((left, A[i] - i));
+        }
+
+        return arr.Max(x => x.left + x.right);
+      }
+    }
+  }
+}
