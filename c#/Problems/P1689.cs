dc541ef6851a300ff1abf47ba39e23c1ed679b87
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-13 13:23:25 +0200

    P1689

---
+++ c#/Problems/P1689.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+  ///    Submission: https://leetcode.com/submissions/detail/430189501/
+  /// </summary>
+  internal class P1689
+  {
+    public class Solution {
+      public int MinPartitions(string n) {
+        return n.Select(_ => int.Parse(_.ToString())).Max(); 
+      }
+    }
+  }
+}
