5aa0f40d072ecd50577b771a330c7208d58cf329
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-13 13:29:49 +0200

    P1688

---
+++ c#/Problems/P1688.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/count-of-matches-in-tournament/
+  ///    Submission: https://leetcode.com/submissions/detail/430190564/
+  /// </summary>
+  internal class P1688
+  {
+    public class Solution
+    {
+      public int NumberOfMatches(int n)
+      {
+        var ans = 0;
+
+        while (n > 1)
+        {
+          ans += n % 2 == 0 ? n / 2 : (n / 2) + 1;
+          n /= 2;
+        }
+
+        return ans;
+      }
+    }
+  }
+}
