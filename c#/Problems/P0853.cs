88a9192c6fd3404593d0b1db93e4380ac32ea1ff
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-31 18:16:45 +0200

    P0853

---
+++ c#/Problems/P0853.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/car-fleet/
+  ///    Submission: https://leetcode.com/submissions/detail/436840594/
+  /// </summary>
+  internal class P0853
+  {
+    public class Solution
+    {
+      public int CarFleet(int target, int[] position, int[] speed)
+      {
+        if (position.Length == 0)
+          return 0;
+
+        // sort by closest position to target
+        var ps = Enumerable.Range(0, position.Length)
+          .Select(i => (d: target - position[i], s: speed[i]))
+          .OrderBy(a => a.d)
+          .ToArray();
+
+        var ans = 1;
+
+        // arrival time
+        var time = 1.0 * ps[0].d / ps[0].s;
+
+        for (var i = 1; i < ps.Length; i++)
+        {
+          var t = 1.0 * ps[i].d / ps[i].s;
+
+          // if next car arrival time is less than prev car arrival time
+          // then it's the same fleet
+          if (t <= time)
+            continue;
+
+          // otherwise we update current arrival time
+          // and this is a new fleet
+          time = t;
+          ans++;
+        }
+
+        return ans;
+      }
+    }
+  }
+}
