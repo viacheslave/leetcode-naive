de45ca450070674c323a8d49c0597be25011a51b
Viacheslav Zholnovach <vyajol@gmail.com>
2021-01-10 15:06:23 +0200

    P1721

---
+++ c#/Problems/P1720.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/decode-xored-array/
+  ///    Submission: https://leetcode.com/submissions/detail/441134002/
+  /// </summary>
+  internal class P1720
+  {
+    public class Solution
+    {
+      public int[] Decode(int[] encoded, int first)
+      {
+        var ans = new int[encoded.Length + 1];
+        ans[0] = first;
+
+        for (var i = 1; i < encoded.Length + 1; i++)
+          ans[i] = ans[i - 1] ^ encoded[i - 1];
+
+        return ans;
+      }
+    }
+  }
+}
