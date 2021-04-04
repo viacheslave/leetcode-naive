4baf160e293bf50b2ca51293ce61e70514a722e0
Viacheslav Zholnovach <vyajol@gmail.com>
2021-04-04 13:19:22 +0300

    P1814

--- c#/Problems/P1791.cs
+++ c#/Problems/P1791.cs
@@ -22,6 +22,4 @@ namespace LeetCode.Naive.Problems
       }
     }
   }
-    }
-  }
 }
---
+++ c#/Problems/P1814.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Naive.Problems
+{
+  /// <summary>
+  ///    Problem: https://leetcode.com/problems/count-nice-pairs-in-an-array/
+  ///    Submission: https://leetcode.com/submissions/detail/476312877/
+  /// </summary>
+  internal class P1814
+  {
+    public class Solution
+    {
+      public int CountNicePairs(int[] nums)
+      {
+        for (var i = 0; i < nums.Length; ++i)
+          nums[i] = nums[i] - int.Parse(new string(nums[i].ToString().Reverse().ToArray()));
+
+        var freq = new Dictionary<int, int>();
+        var ans = 0;
+        var mod = 1_000_000_007;
+
+        for (var i = 0; i < nums.Length; ++i)
+        {
+          if (freq.ContainsKey(nums[i]))
+          {
+            ans += freq[nums[i]];
+            ans %= mod;
+          }
+
+          if (freq.ContainsKey(nums[i]))
+            freq[nums[i]]++;
+          else
+            freq[nums[i]] = 1;
+        }
+
+        return ans;
+      }
+    }
+  }
+}
